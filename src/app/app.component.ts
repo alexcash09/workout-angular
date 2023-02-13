95333fc75d2d785bd43c0fcd77bc67ab033f15e1
Alex Jordan <alexcash09@gmail.com>
2023-02-13 04:30:40 -0700

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,69 @@
 import { Component } from '@angular/core';
 
+const liftDataset = [
+  {
+    exercise: 'bench',
+    sets: '3',
+    reps: '10',
+  },
+  {
+    exercise: 'shoulder press',
+    sets: '3',
+    reps: '10',
+  },
+  {
+    exercise: 'rope pushdowns',
+    sets: '3',
+    reps: '10',
+  },
+  {
+    exercise: 'squat',
+    sets: '5',
+    reps: '10,10,8,6,4',
+  },
+  {
+    exercise: 'pullups x bent over row',
+    sets: '3',
+    reps: '10,8,6',
+  },
+];
+
+const cardioDataset = [
+  {
+    exercise: 'run',
+    time: null,
+    distance: '1 mile',
+  },
+  {
+    exercise: 'row',
+    time: '15:00',
+    distance: null,
+  },
+];
+
+interface TableConfig {
+  dataSource: any;
+  displayedColumns: string[];
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'workout-angular';
+  liftTableConfig: TableConfig;
+  cardioTableConfig: TableConfig;
+  selected: Date | null | undefined;
+
+  constructor() {
+    this.liftTableConfig = {
+      dataSource: liftDataset,
+      displayedColumns: ['exercise', 'sets', 'reps', 'update'],
+    };
+    this.cardioTableConfig = {
+      dataSource: cardioDataset,
+      displayedColumns: ['exercise', 'time', 'distance'],
+    };
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,48 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatCardModule } from '@angular/material/card';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatTableModule } from '@angular/material/table';
+import { MatButtonModule } from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { FooterComponent } from './components/footer/footer.component';
+import { WorkoutComponent } from './features/workout/workout.component';
+import { CalendarComponent } from './features/calendar/calendar.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    WorkoutComponent,
+    CalendarComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    RouterModule.forRoot([
+      { path: 'workout', component: WorkoutComponent },
+      { path: 'calendar', component: CalendarComponent },
+    ]),
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatDividerModule,
+    MatTableModule,
+    MatButtonModule,
+    MatIconModule,
+    MatMenuModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
